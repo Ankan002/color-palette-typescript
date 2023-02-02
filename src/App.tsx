2798b0b353e45fefb4f2ff1386619a138f5cf1b7
Ankan Bhattacharya <ankanbhattacharya89@gmail.com>
2023-02-02 18:31:22 +0530

    chore: :tada: project initialized

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+function App() {
+    return (
+        <main className="min-h-screen w-full flex flex-col justify-center items-center">
+            <h1 className="text-3xl font-quicksand font-bold text-primary-dark">
+                Color Palette Generator
+            </h1>
+        </main>
+    );
+}
+
+export default App;
